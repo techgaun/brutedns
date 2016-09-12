978ae58b1fa300e8d5759858f323b3d0f69ee00d
techgaun <coolsamar207@gmail.com>
2016-09-11 21:00:39 -0500

    initial argparsing

--- brutedns.py
+++ brutedns.py
@@ -1,11 +1,29 @@
 # -*- encoding: utf-8 -*-
 
-import dnspython
+import dns.query
 import argparse
+
+__version__ = "0.0.1"
 
 
 def main():
     print('WIP Parallel DNS Bruteforce')
+    parser = argparse.ArgumentParser(
+        version=__version__,
+        description='brutedns - Parallel DNS (CNAME) Bruteforce Tool',
+        epilog='Use responsibly, Enjoy pentesting'
+    )
+
+    parser.add_argument(
+        '-w',
+        '--wordlist',
+        dest='wordlist',
+        action='store',
+        help='newline separated wordlist. Eg: github-dorks.txt'
+    )
+
+    args = parser.parse_args()
+    print(args)
 
 if __name__ == '__main__':
     main()
